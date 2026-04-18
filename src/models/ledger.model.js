a55b5a6815a1b48229ebe07c79919b5a6144360b
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-04-18 20:03:45 +0530

    Changes

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -45,6 +45,9 @@ ledgerSchema.pre("updateOne", preventLedgerModification);
 ledgerSchema.pre("deleteOne", preventLedgerModification);
 ledgerSchema.pre("remove", preventLedgerModification);
 ledgerSchema.pre("deleteMany", preventLedgerModification);
+ledgerSchema.pre("updateMany", preventLedgerModification);
+ledgerSchema.pre("findOneAndDelete", preventLedgerModification);
+ledgerSchema.pre("findOneAndReplace", preventLedgerModification);
 
 const ledgerModel = mongoose.model("ledger", ledgerSchema);
 
