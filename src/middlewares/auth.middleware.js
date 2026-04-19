6d8db7674ed98ac9eb90fdd233594a76a957dadb
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-04-19 12:35:58 +0530

    Auth System User Middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -24,3 +24,33 @@ export async function authMiddleware(req, res, next) {
     });
   }
 }
+
+export async function authSystemUserMiddleware(req, res, next) {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorised access, token is missing",
+    });
+  }
+
+  try {
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.findById(decoded.userId).select("+systemUser");
+
+    if (!user.systemUser) {
+      return res.status(403).json({
+        message: "Forbidden access,not a system user",
+      });
+    }
+
+    req.user = user;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorised access, token is invalid",
+    });
+  }
+}
